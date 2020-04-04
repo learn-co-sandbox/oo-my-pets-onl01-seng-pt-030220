f1ccebe327a950dac34fc5b42fef9644cd830fd1
Nikelson Exantus <nikelsone1981@hotmail.com>
2020-04-04 16:04:23 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,7 +2,7 @@ class Dog
   # code goes here
   @@all = []
   attr_accessor :owner, :mood
-  attr_accessor :name 
+  attr_reader :name 
   
   def initialize(name, owner, mood = "nervous")
     @name, @owner, @mood = name, owner, mood
