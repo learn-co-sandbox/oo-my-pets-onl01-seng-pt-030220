09165419faddba9ea7d216c8325618c837215285
Nikelson Exantus <nikelsone1981@hotmail.com>
2020-04-04 14:58:44 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,10 @@
 class Dog
   # code goes here
+  attr_accessor :name 
+  
+  def initialize(name, owner)
+    @name, @owner = name, owner
+    
+  end 
+  
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,71 @@
 class Owner
   # code goes here
+  attr_reader :name, :species
+  @@all = []
+  
+  def self.all
+    @all 
+  end 
+  
+  def initialize(name)
+    @name = name 
+    @species = "human"
+    save
+    
+  end 
+  
+  def save
+    @@all << self 
+  end 
+
+
+  def dogs 
+    Dog.all.select{|dog| dog.owner == self}
+  end 
+  
+  def cats 
+     Cat.all.select{|cat| cat.owner == self}
+  end 
+   
+  def walk_dogs 
+     
+  end 
+   
+  def feed_cats 
+     
+  end 
+   
+  def sell_pets 
+     
+  end 
+   
+  def list_pets
+     
+  end 
+  
+  def buy_cat(name)
+    @name
+    
+  end 
+  
+  def buy_dog(name)
+    @name
+    
+  end 
+   
+  def say_species
+    return "I am a human."
+  end 
+  
+  def self.all
+    @@all 
+  end 
+  
+  def self.count 
+    @@all.size
+  end 
+  
+  def self.reset_all
+     @@all.clear
+   end 
 end
