19e73853b409f4843532b74c1ffc12ecba986661
Nikelson Exantus <nikelsone1981@hotmail.com>
2020-04-04 15:57:18 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,8 +1,9 @@
 class Cat
   # code goes here
   attr_accessor :owner, :mood 
+  attr_reader :name
   
-  def initialize(name, owner, mood)
+  def initialize(name, owner)
     @name, @owner, @mood = name, owner, mood
   end
   
