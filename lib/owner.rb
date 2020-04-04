4be8479fc3034d47daac6832eb489d2c5219e76b
Nikelson Exantus <nikelsone1981@hotmail.com>
2020-04-04 17:35:20 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -42,7 +42,7 @@ class Owner
   end 
    
   def list_pets
-    "I have #{self.dogs.count}dog(s), and #{self.cats.count} cat(s)."
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
   end 
   
   def buy_cat(name)
